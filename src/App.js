a4c5e9c26aec854877ef7ef7e2d86b72102b4a34
mxgncraig <megansmcraig@gmail.com>
2023-04-01 21:59:20 +1000

    Created defualt city props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="container">
-      <Weather />
+      <Weather defaultCity="London" />
       <p className="footer">
         This application was coded by{" "}
         <a href="https://github.com/mxgncraig">Megan Craig</a> and is
--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "./Weather.css";
 import Search from "./Search";
 
-export default function Weather() {
+export default function Weather(props) {
   const [ready, setReady] = useState(false);
   const [weather, setWeather] = useState({});
 
@@ -60,8 +60,7 @@ export default function Weather() {
     );
   } else {
     let apiKey = "5201594abea9f3e38b70e65b11a80c24";
-    let city = "London";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading...";
