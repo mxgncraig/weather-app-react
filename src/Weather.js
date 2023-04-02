c442fa8f858f12fdfe4a39cc93a2b0eb63473cfa
mxgncraig <megansmcraig@gmail.com>
2023-04-02 21:03:52 +1000

    Created functioning search engine, removed from own component

--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
-import Search from "./Search";
 import FormattedDate from "./FormattedDate";
 import WeatherData from "./WeatherData";
 
 export default function Weather(props) {
   const [ready, setReady] = useState(false);
   const [weather, setWeather] = useState({});
+  const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
     setWeather({
@@ -23,21 +23,42 @@ export default function Weather(props) {
     setReady(true);
   }
 
+  function search() {
+    const apiKey = "5201594abea9f3e38b70e65b11a80c24";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (ready) {
     return (
       <div className="Weather">
         <p className="time-date">
           <FormattedDate date={weather.date} />
         </p>
-        <Search />
+        <div className="search-engine">
+          <form onSubmit={handleSubmit}>
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Search for a new city..."
+              onChange={handleCityChange}
+            />
+          </form>
+        </div>
         <WeatherData data={weather} />
       </div>
     );
   } else {
-    const apiKey = "5201594abea9f3e38b70e65b11a80c24";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
+    search();
     return "Loading...";
   }
 }
