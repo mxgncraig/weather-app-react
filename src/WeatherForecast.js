95cee20b66d4ab7ea1a4a8b4bf04534e8441a729
mxgncraig <megansmcraig@gmail.com>
2023-04-05 22:07:50 +1000

    Created bulk of forecast interface

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,10 +1,64 @@
 import React from "react";
+import WeatherData from "./WeatherData";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
   return (
     <div className="WeatherForecast">
       <p className="upcoming-days">The next 5 days...</p>
-      <div className="next-five-days"></div>
+      <div className="next-five-days">
+        <div className="row">
+          <div className="col">
+            <div className="WeatherForecast-days">Day</div>
+            <div className="WeatherForecast-icons">
+              <img src="" />
+            </div>
+            <div className="WeatherForecast-temperatures">
+              <span className="WeatherForecast-temperaturesLo">Lo /</span>
+              <span className="WeatherForecast-temperaturesHi">Hi</span>
+            </div>
+          </div>
+          <div className="col">
+            <div className="WeatherForecast-days">Day</div>
+            <div className="WeatherForecast-icons">
+              <img src="" />
+            </div>
+            <div className="WeatherForecast-temperatures">
+              <span className="WeatherForecast-temperaturesLo">Lo /</span>
+              <span className="WeatherForecast-temperaturesHi">Hi</span>
+            </div>
+          </div>
+          <div className="col">
+            <div className="WeatherForecast-days">Day</div>
+            <div className="WeatherForecast-icons">
+              <img src="" />
+            </div>
+            <div className="WeatherForecast-temperatures">
+              <span className="WeatherForecast-temperaturesLo">Lo /</span>
+              <span className="WeatherForecast-temperaturesHi">Hi</span>
+            </div>
+          </div>
+          <div className="col">
+            <div className="WeatherForecast-days">Day</div>
+            <div className="WeatherForecast-icons">
+              <img src="" />
+            </div>
+            <div className="WeatherForecast-temperatures">
+              <span className="WeatherForecast-temperaturesLo">Lo /</span>
+              <span className="WeatherForecast-temperaturesHi">Hi</span>
+            </div>
+          </div>
+          <div className="col">
+            <div className="WeatherForecast-days">Day</div>
+            <div className="WeatherForecast-icons">
+              <img src="" />
+            </div>
+            <div className="WeatherForecast-temperatures">
+              <span className="WeatherForecast-temperaturesLo">Lo /</span>
+              <span className="WeatherForecast-temperaturesHi">Hi</span>
+            </div>
+          </div>
+        </div>
+      </div>
       <p className="warnings">Current weather warnings: None</p>
     </div>
   );
